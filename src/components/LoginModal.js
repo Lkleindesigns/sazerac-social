2752bea3ce21f59c9e286fb24697af0ae43428f3
jeldridgeTS <jeldridge@turbosquid.com>
2020-09-30 14:53:57 -0500

    first pass adding toggle for login/register modal

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import Login from './Login'
+import Register from './Register'
 
 const LoginModal = ({ handleClose, showModal, children }) => {
   const showHideClassName = showModal ? "modal display-block" : "modal display-none";
+  const [ showRegisterForm, setShowRegisterForm ] = useState(false);
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        <Login />
+        <div className="moo-2">
+          { showRegisterForm ? <Register /> : <Login /> }
+        </div>
+        <button className="moo" onClick={() => setShowRegisterForm(!showRegisterForm)}>Register</button>
         <button onClick={() => handleClose()}>close</button>
       </section>
     </div>
