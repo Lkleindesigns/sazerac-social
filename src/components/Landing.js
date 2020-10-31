237b5837ee5c36ee681b19432c4c652a2a450ff0
Logan Klein <lkleindesigns@gmail.com>
2020-10-31 12:15:09 -0500

    Update Landing page, update index.html, delete readme

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Landing = () => {
   return (
     <div>
+      <header>
+        <div>Sazerac Social</div>
+      </header>
       <h1>Landing page</h1>
+      <button>Welcome!</button>
     </div>
   )
 }
